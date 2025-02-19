927be30e5525da069972476b03baec31cfcb4e6e
Lithyem, inc. <michael@lithyem.net>
2025-02-19 12:04:01 -0800

    tweak

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app this rules!!"})
+	return jsonify({"Choo Choo": "Welcome to your Flask app this rules!!"})
 
+@app.route('/test')
+	return jsonify({"YOOOO": "OK"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
