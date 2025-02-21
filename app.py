f33e482d96cd3a8db12973d16e8140d50af6f902
Lithyem, inc. <michael@lithyem.net>
2025-02-20 19:27:12 -0800

    s

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, send_file, render_template
+import os
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/notes')
 def get_notes():
-    return send_file('notes.txt', as_attachment=True)
+    notes_path = os.path.join(os.path.dirname(__file__), 'notes.txt')
+    return send_file(notes_path, as_attachment=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- main.py
+++ main.py
@@ -26,5 +26,7 @@ def testing():
     query_param = request.args.get('param')
     return jsonify({"query_param": query_param})
 
+from app import app
+
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
