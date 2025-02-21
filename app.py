4198e6f3d03b2a39d2f1befa5871139c65d40118
Lithyem, inc. <michael@lithyem.net>
2025-02-20 19:24:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/notes')
 def get_notes():
     return send_file('notes.txt', as_attachment=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
