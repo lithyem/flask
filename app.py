76dab7294fa80f7eefcb4b59460f1db632fd29fa
Lithyem, inc. <michael@lithyem.net>
2025-02-21 08:49:42 -0800

    converted to tabs

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/notes')
 def get_notes():
-    notes_path = os.path.join(os.path.dirname(__file__), 'notes.txt')
-    with open(notes_path, 'r') as file:
-        content = file.read()
-    return Response(content, mimetype='text/plain')
+	notes_path = os.path.join(os.path.dirname(__file__), 'notes.txt')
+	with open(notes_path, 'r') as file:
+		content = file.read()
+	return Response(content, mimetype='text/plain')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from app import app  # Import the Flask app from app.py
 
 @app.route('/test')
 def curious():
-    return jsonify({"YOOOO": "OKman"})
+	return jsonify({"YOOOO": "OKman"})
 
 @app.route('/new_screen')
 def new_screen():
-    return jsonify({"message": "This is a new screen!"})
+	return jsonify({"message": "This is a new screen!"})
 
 @app.route('/himom')
 def himom():
-    return jsonify({"message": "hi mom!"})
+	return jsonify({"message": "hi mom!"})
 
 @app.route('/testing')
 def testing():
-    query_param = request.args.get('param')
-    return jsonify({"query_param": query_param})
+	query_param = request.args.get('param')
+	return jsonify({"query_param": query_param})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
